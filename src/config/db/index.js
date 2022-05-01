e334d114deb17b8d6bb39f3851afd731fe8796a0
haianhnguyen2200 <haianhnguyen2200@gmail.com>
2022-05-01 10:27:59 +0700

    commit 0105

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function connect() {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/education');
-        console.log('Successful');
-    } catch (error) {
-        console.log('Failed');
-    }
+  try {
+    await mongoose.connect("mongodb://localhost:27017/test");
+    console.log("Successful");
+  } catch (error) {
+    console.log("Failed");
+  }
 }
 
 module.exports = { connect };
